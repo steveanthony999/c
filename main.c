8ffceeb8ac82dd4b43215f3441f41f7d671291d7
Steven Woodward <steveanthony999@gmail.com>
2023-02-23 21:20:18 -0800

    added arithmetic

--- main.c
+++ main.c
@@ -1,9 +1,53 @@
 #include <stdio.h>
 
 // Hello, World!
+// int main() {
+//     printf("Hello, World!\n");
+
+//     return 0;
+
+// }
+
+
+// Operators
 int main() {
-    printf("Hello, World!\n");
+    int num_1;
+    int num_2;
+    int result;
+
+    printf("\x1b[32m"); // Changes output color
+    // printf("\x1b[107m"); // Changes output background color
+
+    // Arithmetic
+
+    printf("\nEnter an integer for num_1: ");
+    scanf("%d", &num_1);
+
+    printf("\nEnter an integer for num_2: ");
+    scanf("%d", &num_2);
+    
+    // Addition
+    // result = num_1 + num_2;
+
+    // printf("\n%d + %d = %d\n\n", num_1, num_2, result);
+
+
+    // Subtraction
+    // result = num_1 - num_2;
+
+    // printf("\n%d - %d = %d\n\n", num_1, num_2, result);
+
+
+    // Multiplication
+    // result = num_1 * num_2;
+
+    // printf("\n%d * %d = %d\n\n", num_1, num_2, result);
+
+
+    // Division
+    // result = num_1 / num_2;
+
+    // printf("\n%d / %d = %d\n\n", num_1, num_2, result);
 
     return 0;
-
 }
